38ba2349e84f54958ac1faedb64ba63050084344
p47083lt <lucy.topp@student.manchester.ac.uk>
2024-02-12 13:56:43 +0000

    updated connection file to connect to university hosted page

--- connection.php
+++ connection.php
@@ -1,9 +1,16 @@
-<?php
-//Connection credentials
+<?php /*
+/Connection credentials
 $servername = 'localhost';
 $username = 'root';
 $password = 'Lucy05';
 $dbname = 'flicks';
+*/
+
+$database_host = "dbhost.cs.man.ac.uk";
+$database_user = "p47083lt";
+$database_pass = "LucyMegan05";
+$database_name = "2023_comp10120_cm5";
+
 // Create connection
 $con = new mysqli($servername, $username, $password, $dbname, 3307);
 // Check connection
@@ -11,4 +18,7 @@ if ($con->connect_errno) {
   echo "Connection failed";
   exit();
 }
+
+
+
 
