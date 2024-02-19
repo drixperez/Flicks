543b9b5d4443eb6eede8f9dfa48b1d9873ba51ac
p47083lt <lucy.topp@student.manchester.ac.uk>
2024-02-19 15:24:01 +0000

    it works again i was being silly xoxoox

--- login.php
+++ login.php
@@ -31,14 +31,10 @@ if ($con->connect_errno) {
 
         if(!empty($email) && !empty($password))
         {
-          echo"output3";
           //read from database
           $query = $con -> query("SELECT * FROM `user` WHERE `email` = '$email'");
-          //$result = mysqli_query($con, $query);
-          echo"where does it break";
           if($query)
           {
-            echo"here??";
             if($result && mysqli_num_rows($result) > 0)
             {
               $user_data = mysqli_fetch_assoc($result);
