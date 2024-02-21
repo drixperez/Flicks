789b88cd8be8c21c519451f28888dc35070899f8
y98900gb <ghilse.bouaraba@student.manchester.ac.uk>
2024-02-21 21:38:52 +0000

    Updated JS for the pop up boxes

--- settings.js
+++ settings.js
@@ -1,9 +1,14 @@
 function openChangePasswordPopUp() {
     document.getElementById("overlay").style.display = "block";
-    document.getElementById("popup").style.display = "block";
+    document.getElementById("passwordPopUp").style.display = "block";
 }
 
 function closeChangePasswordPopUp() {
     document.getElementById("overlay").style.display = "none";
-    document.getElementById("popup").style.display = "none";
+    document.getElementById("passwordPopUp").style.display = "none";
+}
+
+function openEmailChangePopUp() {
+    //document.getElementById("overlay").style.display = "block";
+    // document.getElementById("")
 }
