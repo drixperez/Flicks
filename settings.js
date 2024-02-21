41d31a5d416612ae08eec1baca20c20ae0b316dc
Ghiles Bouaraba <ghiles.bouaraba@student.manchester.ac.uk>
2024-02-21 17:19:59 +0000

    Upload New File

---
+++ settings.js
@@ -0,0 +1,9 @@
+function openChangePasswordPopUp() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("popup").style.display = "block";
+}
+
+function closeChangePasswordPopUp() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("popup").style.display = "none";
+}
