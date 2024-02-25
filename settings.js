44a829999730736c469b0005c3d53ced941bbf56
y98900gb <ghilse.bouaraba@student.manchester.ac.uk>
2024-02-25 23:04:12 +0000

    Has the code for the pop ups

--- settings.js
+++ settings.js
@@ -9,6 +9,41 @@ function closeChangePasswordPopUp() {
 }
 
 function openEmailChangePopUp() {
-    //document.getElementById("overlay").style.display = "block";
-    // document.getElementById("")
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("emailPopUp").style.display = "block";
+}
+
+function closeEmailChangePopUp() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("emailPopUp").style.display = "none";
+}
+
+function openGenreChangePopUp() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("genrePopUp").style.display = "block";
+}
+
+function closeGenreChangePopUp() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("genrePopUp").style.display = "none";
+}
+
+function openServiceChangePopUp() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("streamingServicePopUp").style.display = "block";
+}
+
+function closeServiceChangePopUp() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("streamingServicePopUp").style.display = "none";
+}
+
+function openLanguageChangePopUp() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("languageChangePopUp").style.display = "block";
+}
+
+function closeLanguageChangePopUp() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("languageChangePopUp").style.display = "none";
 }
