49368fa759e5968d7e89f397148496f9581ea77c
y98900gb <ghilse.bouaraba@student.manchester.ac.uk>
2024-02-26 15:14:20 +0000

    Started the random page

---
+++ randomPage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <title>Random Page</title>
+        <link rel="stylesheet" href="randomPageStyles.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Concert+One">
+        <script src="randomPageScript.js"></script>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <div id="movie1" class="movie1">
+        
+    </div>
